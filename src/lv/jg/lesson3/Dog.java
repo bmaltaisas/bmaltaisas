04b507c5d79bfcb534b1e9de85fb7f1d7403d648
bmaltaisas <wang384984zhao907@163.com>
2024-08-16 15:38:27 +0800

    Dog Object

--- src/lv/jg/lesson3/Dog.java
+++ src/lv/jg/lesson3/Dog.java
@@ -1,20 +1,51 @@
 package lv.jg.lesson3;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import java.awt.*;
+import java.time.LocalDate;
 
 public class Dog {
     private int yearOfBirth;
     private String name;
     private Color color;
 
-    public void voice(){
-        System.out.println("Vau Vau");
-    }
-    public void eat(){
-        System.out.println("Nom-Nom");
-    }
-    public void sleep(){
-        System.out.println("ZzzZzz...");
+
+    public Dog(String name, int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth; // this. - attiecas un visiem konstruktoriem
+        this.name = name;                               // ...= yearOfBirth attiecas tikai uz public Dog(int)
     }
 
+    public Dog(String bob, javafx.scene.paint.Color beige) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public int getAge () {
+        int age = LocalDate.now().getYear() - yearOfBirth;
+        return age;
+    }
+
+    public String getName() {
+//        System.out.println("Enter dogs name: ");
+//        Scanner scanner = new Scanner(System.in);
+//        String name = scanner.next();
+        return this.name;
+    }
+
+    public Color getColor(){
+        return this.color;
+    }
+
+    public void voice () {
+        System.out.println("Vau Vau");
+    }
+
+    public void eat () {
+        System.out.println("Nom-Nom");
+    }
+    public void sleep () {
+        System.out.println("ZzzZzz...");
+    }
 }
+
--- src/lv/jg/lesson3/DogTest.java
+++ src/lv/jg/lesson3/DogTest.java
@@ -1,4 +1,34 @@
 package lv.jg.lesson3;
 
+import javafx.scene.paint.Color;
+
+import java.time.LocalDate;
+
 public class DogTest {
+
+    public static void main(String[] args){
+
+        Dog rex = new Dog("Rex", 2015);
+        rex.eat();
+        rex.sleep();
+        rex.voice();
+
+        System.out.println("Today is year: " + LocalDate.now().getYear());
+        System.out.println("Dog's age is: " + rex.getAge() + " years old");
+
+        doDogRoutine();
+
+
+    }
+
+    private static void doDogRoutine() {
+        Dog bob = new Dog ("Bob", Color.BEIGE);
+        bob.eat();
+        bob.sleep();
+        bob.voice();
+
+        System.out.println("The dog is " + bob.getAge() + "  years old ");
+        System.out.println("My name is " + bob.getName());
+        System.out.println("My color is  " + bob.getColor());
+    }
 }
