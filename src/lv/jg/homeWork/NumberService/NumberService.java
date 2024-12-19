7351359d24a175d8c73cffd3bc595645aba6f3dc
bmaltaisas <wang384984zhao907@163.com>
2024-12-20 07:51:48 +0800

    HomeWork

--- src/lv/jg/homeWork/NumberService/NumberService.java
+++ src/lv/jg/homeWork/NumberService/NumberService.java
@@ -33,27 +33,6 @@ public class NumberService {
         }
         return false;
     }
-
 }
 
-
-//    public void printWithinInterval(int from, int to) {
-//        for (int i = from; i <= to; i++) {              // for (; from < to; from++)
-//            if (isPrimeNumber(i)) {
-//
-//                System.out.println(i + " Is Prime number");
-//            } else {
-//                System.out.println(i + " Doesn't count as Prime number!");
-//            }
-//        }
-//    }
-//
-//    private boolean isPrimeNumber(int number) {
-//        for (int i = 2; i < number; i++) {
-//            if (number % i == 0) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
 
